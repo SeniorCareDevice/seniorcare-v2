0b59a93235b34f7f58d82b201701f3c01e1b8a6c
Senior Care <seniorcaredevice@gmail.com>
2025-03-04 00:33:57 +0530

    Initial commit for seniorcare-v2

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
+
+// Store latest data
+let sensorData = { temperature: 0, acceleration: 0, fallDetected: false };
+let gpsData = { latitude: 0, longitude: 0, gpsValid: false };
+
+// Serve sensor data
+app.get('/data', (req, res) => {
+    res.json(sensorData);
+});
+
+// Serve GPS data
+app.get('/gps', (req, res) => {
+    res.json(gpsData);
+});
+
+// Receive sensor data
+app.post('/data', (req, res) => {
+    sensorData = req.body;
+    console.log('Received sensor data:', sensorData);
+    res.sendStatus(200);
+});
+
+// Receive GPS data
+app.post('/gps', (req, res) => {
+    gpsData = req.body;
+    console.log('Received GPS data:', gpsData);
+    res.sendStatus(200);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
